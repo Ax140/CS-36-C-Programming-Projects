149c45e4c91e293b07e87a944bc5174a7ee1f816
Amin M <aminmontazeri@icloud.com>
2020-12-25 21:24:19 -0800

    Version 1 of the sum and average program
    The value of the variables are predefined in V1

---
+++ Sum and Average V1/cmake-build-debug/CMakeFiles/Sum_and_Average.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sum_and_Average.dir/main.c.obj"
+  "Sum_and_Average.exe"
+  "Sum_and_Average.exe.manifest"
+  "Sum_and_Average.lib"
+  "Sum_and_Average.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sum_and_Average.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
